8e73e1babb33dbaab66f62f539fa702121c698e9
Daniel Platt <dplatt@ukwebmedia.com>
2017-11-14 15:01:56 +0000

    initial skeleton and layout

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction()
+    {
+        return $this->render('index.html.twig', [
+        ]);
+    }
+}
